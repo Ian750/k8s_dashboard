0e8b48ad3aaf096da2c54ce8d8e0004fb48da02d
ianjiang <ruinsky7501@gmail.com>
2021-09-03 17:39:28 +0800

    push k8s_dashboard and TestBlueGreenDep file

---
+++ nodejs/index_red.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors());
+
+var myArray = [
+    '#FF0000'
+];
+var rand = Math.floor(Math.random() * myArray.length);
+var rValue = myArray[rand];
+
+app.get('/color', (req, res) => {
+    res.send(rValue);
+});
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
